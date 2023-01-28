9ccd79e95a5c0a436c6d909c21914f9f564981cb
MitsukuniH <hmitsuku3292@gmail.com>
2023-01-28 14:01:48 +0900

    Initial Commit

---
+++ firstSwift/firstSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstSwiftApp.swift
+//  firstSwift
+//
+//  Created by 東光邦 on 2023/01/28.
+//
+
+import SwiftUI
+
+@main
+struct firstSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
